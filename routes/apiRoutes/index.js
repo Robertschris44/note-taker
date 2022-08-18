d9acd4ae85e2d6eff0949981b70b05112a69aa26
Chris Key <robertschris010@gmail.com>
2022-08-17 19:38:44 -0500

    got server working

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,16 @@
-// //router() is used to create a new router object
-const router = require('express').Router();
+// // //router() is used to create a new router object
+// const router = require('express').Router();
 
-//connected passageway to apiRoutes folder then the js file inside
-const noteRoutes = require('../apiRoutes/notesRoutes');
+// //connected passageway to apiRoutes folder then the js file inside
+// const noteRoutes = require('../apiRoutes/notesRoutes');
 
-router.use("/notes", noteRoutes);
+// router.use("/api/notes", noteRoutes);
+// router.use((req, res) => {
+//     res.status(404).end();
+// })
 
 
-
-//route cannot listen (PORT) for request on its own will set module.exports = to router
-module.exports = router;
+// //route cannot listen (PORT) for request on its own will set module.exports = to router
+// module.exports = router;
 
 
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 //local host server set to 3001
-const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const {notes} = require('./db/db.json');
+const lib = require('./lib/notes');
 const app = express();
-
 //creating const for passage/routes
-const routes = require('./routes');
+// const routes = require('./routes');
+
+const PORT = process.env.PORT || 3002;
 
 
 
@@ -16,9 +20,47 @@ app.use(express.json());
 //static middle ware so page can pickup static files like the style sheet
 app.use(express.static('public'));
 
-app.use(routes);
+
+function newNote(body, messageArray){
+  const note = body
+  messageArray.push(note)
+  fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({notes: messageArray}, null, 2),
+  )
+  return note
+}
+
+
+
+app.get('/api/notes', (req, res) => {
+  let results = notes
+  if(req.query){
+    results = lib.filterByQuery(req.query, results)
+  }
+  res.json(results)
+})
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString()
+  const note = newNote(req.body, notes)
+  res.json(note)
+})
+
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html')) //sending it to directory name
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html')) //sending it to directory name
+})
+
+
 
 //always set listen on bottom 
 app.listen(PORT, () => {
-    console.log(`API server on port ${PORT}!`);
-});
+    console.log(`API server now on port ${PORT}!`);
+  });
